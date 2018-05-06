865d6eb61417fc29db8065d0df18c87702b62168
peterzandbergen <peter.zandbergen@myhops.com>
2018-05-06 13:01:43 +0200

    Added test two to check retry

--- adapters/meter/meter_test.go
+++ adapters/meter/meter_test.go
@@ -2,6 +2,7 @@ package meter
 
 import (
 	"testing"
+	"time"
 
 	"github.com/peterzandbergen/iec62056/iec"
 )
@@ -21,4 +22,26 @@ func TestGet(t *testing.T) {
 		t.Fatalf("Get failed, error: %s", err.Error())
 	}
 	t.Logf("Measurement: %v", *msm)
+}
+
+func TestGetTwo(t *testing.T) {
+	ps := iec.NewDefaultSettings()
+	m := &Meter{
+		PortSettings: ps,
+		PortName:     "/dev/ttyUSB0",
+	}
+	msm, err := m.Get(nil)
+	if err != nil {
+		t.Fatalf("Get failed, error: %s", err.Error())
+	}
+	t.Logf("Measurement 1: %v", *msm)
+
+	// Try again.
+	var st = 10 * time.Second
+	time.Sleep(st)
+	msm, err = m.Get(nil)
+	if err != nil {
+		t.Fatalf("Get failed, error: %s", err.Error())
+	}
+	t.Logf("Measurement 2: %v", *msm)
 }
