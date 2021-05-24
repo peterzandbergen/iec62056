03afead09a9dc4bc4a8517c1d20db1d60fe58ee6
peterzandbergen <peter.zandbergen@myhops.com>
2021-05-24 12:28:18 +0200

    adapt portmon for new meter

--- cmd/portmon/main.go
+++ cmd/portmon/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"sync"
 	"time"
+	"bytes"
 
 	"github.com/peterzandbergen/iec62056/iec/telegram"
 	"go.bug.st/serial.v1"
@@ -14,9 +15,12 @@ import (
 
 func readCommands(wg *sync.WaitGroup, in io.Reader, p io.Writer) {
 	// connect buffered reader and read bytes.
+	b := bytes.Buffer{}
+	telegram.SerializeRequestMessage(&b, telegram.RequestMessage{})
+	reqMsg := b.Bytes()
 	var cr = bufio.NewReader(in)
 	time.Sleep(time.Second)
-	telegram.SerializeRequestMessage(p, telegram.RequestMessage{})
+	// telegram.SerializeRequestMessage(p, telegram.RequestMessage{})
 	for {
 		b, err := cr.ReadByte()
 		if err != nil {
@@ -25,11 +29,24 @@ func readCommands(wg *sync.WaitGroup, in io.Reader, p io.Writer) {
 		switch rune(b) {
 		case 'r', 'R':
 			// Send request.
-			telegram.SerializeRequestMessage(p, telegram.RequestMessage{})
+			fmt.Println("sending request")
+			// n, err := telegram.SerializeRequestMessage(p, reqMsg)
+			n, err := p.Write(reqMsg)
+			if err != nil {
+				fmt.Printf("error: %s\n", err)
+				break
+			} 
+			fmt.Printf("sent request bytes: %d\n", n)
 		case 'a', 'A':
 			// Send request.
+			fmt.Println("sending ACK")
 			ack := "\x06\x00\x00\x00\r\n"
-			p.Write([]byte(ack))
+			n, err := p.Write([]byte(ack))
+			if err != nil {
+				fmt.Printf("error: %s\n", err)
+				break
+			} 
+			fmt.Printf("sent ack bytes: %d\n", n)
 		case 'q', 'Q':
 			wg.Done()
 			return
@@ -62,11 +79,22 @@ func writeHexResponses(wg *sync.WaitGroup, in *bufio.Reader, out io.Writer) {
 		if err != nil {
 			return
 		}
-		fmt.Fprintf(out, "[%c]", b)
+		fmt.Fprintf(out, "[%x]", b)
 		if b == 0x0D {
 			b = ' '
 		}
-		fmt.Fprint(out, rune(b))
+		fmt.Fprint(out, string([]byte{b}))
+	}
+}
+
+func writeLineResponses(wg *sync.WaitGroup, in *bufio.Reader, out io.Writer) {
+	defer wg.Done()
+	for {
+		b, err := in.ReadByte()
+		if err != nil {
+			return
+		}
+		fmt.Fprint(out, string([]byte{b}))
 	}
 }
 
@@ -75,9 +103,9 @@ func main() {
 
 	p, err := serial.Open("/dev/ttyUSB0",
 		&serial.Mode{
-			BaudRate: 300,
-			DataBits: 7,
-			Parity:   serial.EvenParity,
+			BaudRate: 115200,
+			DataBits: 8,
+			Parity:   serial.NoParity,
 			StopBits: serial.OneStopBit,
 		})
 	if err != nil {
@@ -89,9 +117,14 @@ func main() {
 	br := bufio.NewReader(p)
 
 	// Start the reader and the writer.
+	fmt.Println("starting loop")
 	wg.Add(2)
+	// p.SetRTS(true)
 	go readCommands(wg, os.Stdin, p)
-	go writeResponses(wg, br, os.Stdout)
+	// go writeResponses(wg, br, os.Stdout)
 	// go writeHexResponses(wg, br, os.Stdout)
+	go writeLineResponses(wg, br, os.Stdout)
+	p.SetRTS(false)
+	// p.SetDTR(true)
 	wg.Wait()
 }
